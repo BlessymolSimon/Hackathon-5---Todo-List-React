6e8da0bc6ff96650e2969b1d5d7b3f1083e3c606
Blessymol Simon <blessy4mooc@gmail.com>
2020-11-23 22:40:38 +0530

    working well

--- src/components/App.js
+++ src/components/App.js
@@ -1,77 +1,80 @@
 import React, { useState } from 'react';
 import "./../styles/App.css";
 
-function App() 
-{
-	const[input,setInput] = useState("");
-	const[list,setList] = useState([]);
-	const [editMode,setEditMode] = useState([]);
+function App() {
+	const [input, setInput] = useState("");
+	const [editItem, setItem] = useState("");
+	const [list, setList] = useState([]);
+	const [editMode, setEditMode] = useState([]);
 	function handleChange(evt) {
 		setInput(evt.target.value);
 	}
+	function handleEdit(evt) {
+		setItem(evt.target.value);
+	}
 	function addNewTask() {
-		if(input.length > 0) {
-			const new_list=[...list];
+		if (input.length > 0) {
+			const new_list = [...list];
 			new_list.push(input);
-			console.log("new list",new_list);
+			console.log("new list", new_list);
 			setList(new_list);
-			const new_editMode=[...editMode,0];
+			const new_editMode = [...editMode, 0];
 			setEditMode(new_editMode);
 			setInput("");
-			console.log("new edit mode array",new_editMode);
-		}	
+			console.log("new edit mode array", new_editMode);
+		}
 	}
 	function editor(index) {
-		const new_editMode=[...editMode];
-		new_editMode[index]=1;
+		const new_editMode = [...editMode];
+		new_editMode[index] = 1;
 		setEditMode(new_editMode);
 	}
 	function updateTask(index) {
-		const new_list=[...list];
-		new_list[index]=input;
-		setList(new_list);
-		const new_editMode=[...editMode];
-		new_editMode[index]=0;
+		if (editItem.length > 0) {
+			const new_list = [...list];
+			new_list[index] = editItem;
+			setList(new_list);
+		}
+		const new_editMode = [...editMode];
+		new_editMode[index] = 0;
 		setEditMode(new_editMode);
-		setInput("");
+		setItem("");
 	}
 	function deletion(index) {
 		// console.log(index);
-		const arr=list.filter((item,ind) => ind !== index);
+		const arr = list.filter((item, ind) => ind !== index);
 		// console.log(`array after filtering ${arr}`);
 		setList(arr);
-		const new_editMode=editMode.filter((item,ind) => ind !== index);
+		const new_editMode = editMode.filter((item, ind) => ind !== index);
 		setEditMode(new_editMode);
 	}
 	return (
-	<div id="main">
-		<input type="text" id="task" onChange={handleChange} value={input}></input>
-		<button id="btn" onClick={addNewTask}>Add</button>
-		<ul>{
-				list.map((item,ind) =>
-					{
-						// console.log("within list.map",editMode[ind]===0,item,ind);
-						return(
-							<div> {
-						(editMode[ind] === 0) ?  // not editable
-						(<li key={ind} className="list">
-							<input type="text" value={item} disabled></input>
-							<button className="edit" onClick={() => editor(ind)}>Edit</button>
-							<button className="delete" onClick={() => deletion(ind)}>Delete</button>					
-						</li>)
-						: 
-						(// setInput(item)
-						<li key={ind} className="list">
-							<input className="editTask" type="text" value={item} onChange={handleChange}></input>
-							<button className="saveTask" onClick={() => updateTask(ind)}>Save</button>
-						</li>)}
+		<div id="main">
+			<input type="text" id="task" onChange={handleChange} value={input}></input>
+			<button id="btn" onClick={addNewTask}>Add</button>
+			<ul>{
+				list.map((item, ind) => {
+					// console.log("within list.map",editMode[ind]===0,item,ind);
+					return (
+						<div> {
+							(editMode[ind] === 0) ?  // not editable
+								(<li key={ind} className="list">
+									<input type="text" value={item} disabled></input>
+									<button className="edit" onClick={() => editor(ind)}>Edit</button>
+									<button className="delete" onClick={() => deletion(ind)}>Delete</button>
+								</li>)
+								:
+								(// setInput(item)
+									<li key={ind} className="list">
+										<input className="editTask" type="text" onChange={handleEdit}></input>
+										<button className="saveTask" onClick={() => updateTask(ind)}>Save</button>
+									</li>)}
 						</div>);
-					}
+				}
 				)
 			}
-		</ul>
-	</div>
+			</ul>
+		</div>
 	);
 }
-
 export default App;
